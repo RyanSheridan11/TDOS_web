47adca81a12f01f6f873966bd2f615c5b56bb5dc
Ryan-Sheridan <ryansheridan11@gmail.com>
2020-11-05 16:49:25 +1300

    Got the bubble hover background working on test

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,101 +1,140 @@
 <template>
     <div class="test">
-        <Particles id="tsparticles"
-            :options="{
-                background: {
-                    color: '#fafafa'
-                },
-                fpsLimit: 60,
-                interactivity: {
-                    detectsOn: 'canvas',
-                    events: {
-                        onClick: {
-                            enable: true,
-                            mode: 'push'
-                        },
-                        onHover: {
-                            enable: true,
-                            mode: 'repulse'
-                        },
-                        resize: true
-                    },
-                    modes: {
-                        bubble: {
-                            distance: 400,
-                            duration: 2,
-                            opacity: 0.8,
-                            size: 40
-                        },
-                        push: {
-                            quantity: 4
-                        },
-                        repulse: {
-                            distance: 200,
-                            duration: 0.4
-                        }
-                    }
-                },
-                particles: {
-                    color: {
-                        value: '#ffffff'
-                    },
-                    links: {
-                        color: '#ffffff',
-                        distance: 150,
-                        enable: true,
-                        opacity: 0.5,
-                        width: 1
-                    },
-                    collisions: {
-                        enable: true
-                    },
-                    move: {
-                        direction: 'none',
-                        enable: true,
-                        outMode: 'bounce',
-                        random: false,
-                        speed: 6,
-                        straight: false
-                    },
-                    number: {
-                        density: {
-                            enable: true,
-                            value_area: 800
-                        },
-                        value: 80
-                    },
-                    opacity: {
-                        value: 0.5
-                    },
-                    shape: {
-                        type: 'circle'
-                    },
-                    size: {
-                        random: true,
-                        value: 5
-                    }
-                },
-                detectRetina: true
-        }" />
+			<Particles
+			id="tsparticles"
+				:options="{
+					fpsLimit: 60,
+					interactivity: {
+						detectsOn: 'canvas',
+						events: {
+							onClick: {
+								enable: true,
+								mode: 'remove'
+							},
+							onHover: {
+								enable: true,
+								mode: 'bubble'
+							},
+							resize: true
+						},
+						modes: {
+							bubble: {
+								distance: 400,
+								duration: 2,
+								opacity: 1,
+								size: 150,
+								speed: 3
+							},
+							push: {
+								quantity: 4
+							},
+							remove: {
+								quantity: 4
+							}
+						}
+					},
+					particles: {
+						number: {
+							value: 80,
+							density: {
+								enable: true,
+								value_area: 800
+							},
+						},
+						color: {
+							value: '#000000'
+						},
+						shape: {
+							type: 'circle',
+							stroke: {
+								width: 0,
+								color: '#000000'
+							},
+							polygon: {
+								nb_sides: 5
+							},
+							image: {
+								src: '@/assets/table.jpg',
+								width: 100,
+								height: 100
+							}
+						},
+						opacity: {
+							value: 0.7,
+							random: true,
+							anim: {
+								enable: true,
+								speed: 1,
+								opacity_min: 0.1,
+								sync: false
+							}
+						},
+						size: {
+							value: 100,
+							random: true,
+							anim: {
+								enable: true,
+								speed: 40,
+								size_min: 0.1,
+								sync: false
+							}
+						},
+						line_linked: {
+							enable: false,
+							distance: 150,
+							color: '#ffffff',
+							opacity: 1,
+							width: 1
+						},
+						move: {
+							enable: true,
+							speed: 2,
+							direction: 'none',
+							random: false,
+							straight: false,
+							outMode: 'out',
+							attract: {
+								enable: false,
+								rotateX: 600,
+								rotateY: 1200
+							}
+						},
+
+
+					},
+					background: {
+						image: 'url(assets/table.jpg)'
+					},
+
+					detectRetina: true
+				}"
+			/>
+			<img alt="Our first table" src="@/assets/table.jpg">
     </div>
 </template>
 
 
 <style>
     #tsparticles {
-        z-index: -100;
+        z-index: 2;
         position: fixed;
         left: 0;
         top: 0;
         width: 100vw;
         height: 100vh;
 
-        background-color: white;
-        background-image: url("");
         background-repeat: no-repeat;
         background-size: cover;
         background-position: 50% 50%;
     }
+		img {
+			position: fixed;
+			left: 0;
+			top: 0;
+			width: 100vw;
+			height: 100vh;
+			z-index: 1;
+		}
     header, footer {
         visibility: hidden;
     }
